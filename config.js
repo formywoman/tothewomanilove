e8f3157f050ad11284c6a5788409108f5ae3f2d2
racevoltvs <racevolt@gmail.com>
2026-02-11 00:03:45 +0400

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dtnadrthx/video/upload/v1770753747/love_me_harder_-_tiktok_edit_audio_version_so_what_would_I_do_if_I_can_t_figure_it_out__psva82.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
